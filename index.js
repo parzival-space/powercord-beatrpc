880810c7221a3a8552b7e9997e65871ed19b579f
Parzival <malte.linke.ml@gmail.com>
2022-01-09 01:53:56 +0100

    difficulty indicator, BeatSaber button

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ const { WebSocket } = require("./modules/ws");
 // get set_activity
 const { SET_ACTIVITY } = getModule(["SET_ACTIVITY"], false);
 
+// node native modules
+const https = require('https');
+
 // store rpc data
 var rpc = {
   client_id: "923576268765687818", // discord client id
   name: "Beat Saber", // name of the game
   state: "Taking a break", // song author
   details: "Main Menu", // song title
+  small_image: undefined, // difficulty icon
+  small_text: undefined, // difficulty name
+  buttons: undefined, // buttons (BeatSaver)
 };
 
 // stores
@@ -28,7 +34,7 @@ module.exports = class BeatRPC extends Plugin {
     ws = new WebSocket("ws://localhost:6557/socket");
 
     // handle websocket events
-    ws.addEventListener("message", (event) => {
+    ws.addEventListener("message", async (event) => {
       // convert data
       let data = {};
 
@@ -52,6 +58,17 @@ module.exports = class BeatRPC extends Plugin {
         // update rpc data
         rpc.details = data.status.beatmap.songName;
         rpc.state = data.status.beatmap.songAuthorName;
+        rpc.small_image = this.getDifficultyIcon(data.status.beatmap.difficultyEnum);
+        rpc.small_text = data.status.beatmap.difficultyEnum;
+
+        if (data.status.beatmap.songHash) {
+          // get song by hash
+          let song = await this.getSongByHash(data.status.beatmap.songHash)
+            .catch(_ => console.log("%c[BeatRPC]%c Could not get song by hash", "color: #ff0000;", "color: #ffffff;"));
+
+          // add button
+          rpc.buttons = [{ label: 'View on BeatSaver', url: `https://beatsaver.com/maps/${song.id}` }];
+        }
 
         // no forced rpc update required
       }
@@ -61,6 +78,9 @@ module.exports = class BeatRPC extends Plugin {
         // update rpc data
         rpc.details = "Main Menu";
         rpc.state = "Taking a break";
+        rpc.small_image = undefined;
+        rpc.small_text = undefined;
+        rpc.buttons = undefined;
 
         // no forced rpc update required
       }
@@ -86,7 +106,7 @@ module.exports = class BeatRPC extends Plugin {
         "color: #ffffff;"
       );
 
-      // try to reconnect after 2 seconds
+      // try to reconnect after 5 seconds
       if (rt) return;
       rt = setInterval(() => {
         this.pluginWillUnload();
@@ -103,6 +123,9 @@ module.exports = class BeatRPC extends Plugin {
       // reset rpc data if they are for whatever reason not default
       rpc.details = "Main Menu";
       rpc.state = "Taking a break";
+      rpc.small_image = undefined;
+      rpc.small_text = undefined;
+      rpc.buttons = undefined;
 
       // start the interval
       if (!ut) ut = setInterval(() => this.updateRPC(), 2000);
@@ -144,10 +167,11 @@ module.exports = class BeatRPC extends Plugin {
           timestamps: undefined,
           assets: {
             large_image: "game", // game logo
-            small_image: undefined, // not needed
+            small_image: rpc.small_image, // not needed
             large_text: undefined, // not needed
             small_text: undefined, // not needed
           },
+          buttons: rpc.buttons,
         },
       },
     });
@@ -169,6 +193,66 @@ module.exports = class BeatRPC extends Plugin {
         pid: 10,
         activity: undefined,
       },
+    });
+  }
+
+  // get difficulity icon name from difficulty name
+  getDifficultyIcon(difficulty) {
+    switch (difficulty) {
+      case "Easy":
+        return "easy";
+
+      case "Normal":
+        return "normal";
+
+      case "Hard":
+        return "hard";
+
+      case "Expert":
+        return "expert";
+
+      case "ExpertPlus":
+        return "expertplus";
+
+      default:
+        return null;
+    }
+  }
+
+  // get current song song from beatsaver by hash
+  getSongByHash(hash) {
+    return new Promise((resolve, reject) => {
+      let options = {
+        rejectUnauthorized: false,
+        host: "api.beatsaver.com",
+        path: `/maps/hash/${hash}`,
+        method: "GET",
+        port: 443,
+      }
+
+      https.get(options, (res) => {
+        let data = '';
+
+        // collect data
+        res.on('data', (chunk) => data += chunk);
+
+        // parse data
+        res.on('end', () => {
+          let result = data;
+
+          // parse data
+          try {
+            result = JSON.parse(data);
+          }
+          catch (e) {
+            // do nothing
+          }
+
+          resolve(result);
+        }).on('error', (e) => {
+          reject(e);
+        });
+      })
     });
   }
 };
